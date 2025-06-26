6db4f14deae635e6471fadbe26f812d548429008
Genesis Barrios <genesisbarriosdev@gmail.com>
2025-06-26 00:32:31 -0400

    spotify

--- src/SOLYMAR.js
+++ src/SOLYMAR.js
@@ -161,7 +161,7 @@ const SOLYMAR = (props) => {
               <YoutubeLogo className="logoSize" style={{ color: 'red', marginRight: "5px" }} size={50} /><p style={{ marginRight: "5px", fontWeight:"600"}}>YouTube</p>
             </Grid>
             <Grid item xs={6} sm={6}>
-            <a target="_blank" href="https://Venice.lnk.to/solymar"><button className="pre-save-button">Stream</button></a>
+            <a target="_blank" href="https://www.youtube.com/watch?v=933mrl_VLyE&list=PLcGBMxEyx5p9cfbXNscaNm6tjNaOoWGBP"><button className="pre-save-button">Stream</button></a>
             </Grid>
           </Grid>
             <Grid container spacing={2} className="logo-button-container">
@@ -169,7 +169,7 @@ const SOLYMAR = (props) => {
               <SoundcloudLogo className="logoSize" style={{ color: '#FF7000', marginRight: "5px" }} size={50} /><p style={{marginRight: "5px", fontWeight:"600"}}>Soundcloud</p>
             </Grid>
             <Grid item xs={6} sm={6}>
-            <a target="_blank" href="https://Venice.lnk.to/solymar"><button className="pre-save-button">Stream</button></a>
+            <a target="_blank" href="https://soundcloud.com/genwav/sets/solymar-3?si=455c02308de54148a00be23130555718&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"><button className="pre-save-button">Stream</button></a>
             </Grid>
           </Grid>
           <Grid container spacing={2} className="logo-button-container">
@@ -177,7 +177,7 @@ const SOLYMAR = (props) => {
               <TidalLogo className="logoSize" style={{ color: 'white', marginRight: "5px" }} size={50} /><p style={{marginRight: "5px", fontWeight:"600"}}>Tidal</p>
             </Grid>
             <Grid item xs={6} sm={6}>
-            <a target="_blank" href="https://Venice.lnk.to/solymar"><button className="pre-save-button">Stream</button></a>
+            <a target="_blank" href="https://tidal.com/browse/album/442067668"><button className="pre-save-button">Stream</button></a>
             </Grid>
           </Grid>
            <Grid container spacing={2} className="logo-button-container">
@@ -185,7 +185,7 @@ const SOLYMAR = (props) => {
              <img className="logoSize" style={{marginRight: "5px" }} src="pandora.png" width={50}></img><p style={{marginRight: "5px", fontWeight:"600"}}>Pandora</p>
             </Grid>
             <Grid item xs={6} sm={6}>
-            <a target="_blank" href="https://Venice.lnk.to/solymar"><button className="pre-save-button">Stream</button></a>
+            <a target="_blank" href="https://pandora.app.link/GMQOTsZnvUb"><button className="pre-save-button">Stream</button></a>
             </Grid>
           </Grid>
            <Grid container spacing={2} className="logo-button-container">
@@ -193,7 +193,7 @@ const SOLYMAR = (props) => {
               <AmazonLogo className="logoSize" style={{ color: '#FF7000', marginRight: "5px" }} size={50} /><p style={{marginRight: "5px", fontWeight:"600"}}>Amazon</p>
             </Grid>
             <Grid item xs={6} sm={6}>
-            <a target="_blank" href="https://Venice.lnk.to/solymar"><button className="pre-save-button">Stream / Buy</button></a>
+            <a target="_blank" href="https://www.amazon.com/music/player/albums/B0FD7FD95M"><button className="pre-save-button">Stream / Buy</button></a>
             </Grid>
           </Grid>
            <Grid container spacing={2} className="logo-button-container">
