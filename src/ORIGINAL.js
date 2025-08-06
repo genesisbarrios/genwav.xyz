d1fa6f8dc5bddfbddd6d8fe489e3ced934d3e981
Genesis Barrios <genesisbarriosdev@gmail.com>
2025-08-06 00:46:55 -0400

    original with kiento

--- src/ORIGINAL.js
+++ src/ORIGINAL.js
@@ -111,7 +111,7 @@ const ORIGINAL = (props) => {
           </Grid>
          <Grid container spacing={2} className="logo-button-container">
             <Grid item xs={6} sm={6} style={{display:"flex", alignItems: "center"}}>
-              <AppleLogo className="logoSize" style={{ color: '#FA4C64', marginRight: "5px" }} size={50} /><p style={{marginRight: "5px", fontWeight:"600", fontSize:"0.8em"}}>Apple Music</p>
+              <AppleLogo className="logoSize" style={{ color: '#FA4C64', marginRight: "5px" }} size={50} /><p style={{marginRight: "5px", fontWeight:"600", fontSize:"0.8em"}}>Apple</p>
             </Grid>
             <Grid item xs={6} sm={6}>
               <a target="_blank" href="https://music.apple.com/us/album/original-feat-gen-wav-single/1829755778"><button className="pre-save-button">Stream</button></a>
